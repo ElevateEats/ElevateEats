72d1d922c6e2f667704c45bcc1f8af3b4f748847
ralleyne531 <ralleyne531@gmail.com>
2024-12-04 15:48:09 -0500

    Hiding secrets

--- ElevateEatsAPI/loggerConfig.js
+++ ElevateEatsAPI/loggerConfig.js
@@ -4,8 +4,8 @@ const LokiTransport = require('winston-loki');
 const logger = winston.createLogger({
     transports: [
         new LokiTransport({
-            host: 'https://<your-loki-endpoint>',
-            basicAuth: , // Use your Grafana Cloud API key
+            host: 'https://logs-prod-006.grafana.net',
+            basicAuth: `:${process.env.LOKI_API_TOKEN}`, // Use your Grafana Cloud API key
             labels: { app: 'ElevateEats Back-End' },
         }),
     ],
