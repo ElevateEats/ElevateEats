b25809d00d21416bde1f9f8abc28b58be168ae98
ralleyne531 <ralleyne531@gmail.com>
2025-04-08 20:09:05 -0400

    Adding script to generate API key for given user

---
+++ ElevateEatsAPI/scripts/generateKeyForUser.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { v4 as uuidv4 } from 'uuid';
+import ApiKey from '../schema/apiKeySchema.js';
+import User from '../schema/userSchema.js';
+
+dotenv.config();
+await mongoose.connect(process.env.MONGODB_URI);
+
+const user = await User.findOne({ email: 'ralleyne531@gmail.com' });
+if (!user) {
+    console.log('User not found');
+    process.exit();
+}
+
+const newKey = new ApiKey({
+    key: uuidv4(),
+    userID: user._id,
+    isActive: true,
+});
+
+await newKey.save();
+console.log('🔑 API key created for user:', newKey.key);
+process.exit();
