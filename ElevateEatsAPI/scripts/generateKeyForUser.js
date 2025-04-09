e641b8bad54284ea06fffc3d2271f510c3d09adc
ralleyne531 <ralleyne531@gmail.com>
2025-04-08 20:24:37 -0400

    adding apiKey for test user "johnDoe"

--- ElevateEatsAPI/scripts/generateKeyForUser.js
+++ ElevateEatsAPI/scripts/generateKeyForUser.js
@@ -7,7 +7,7 @@ import User from '../schema/userSchema.js';
 dotenv.config();
 await mongoose.connect(process.env.MONGODB_URI);
 
-const user = await User.findOne({ phoneNumber: 'x' });
+const user = await User.findOne({ phoneNumber: '1234567890' });
 if (!user) {
     console.log('User not found');
     process.exit();
