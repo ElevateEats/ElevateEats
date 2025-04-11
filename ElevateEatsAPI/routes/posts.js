a36eec9964d28910403aa48dc18cbab959d98283
ralleyne531 <ralleyne531@gmail.com>
2025-04-10 23:57:57 -0400

    Installing and running a prettier engine

--- ElevateEatsAPI/routes/posts.js
+++ ElevateEatsAPI/routes/posts.js
@@ -1,108 +1,108 @@
-import express from 'express'
+import express from "express";
 // import { getUsers } from '../controllers/postController.js'
 import Post from "../schema/postSchema.js";
 import logger from "../utils/logger.js";
 
-const router = express.Router()
-router.use(express.json())
+const router = express.Router();
+router.use(express.json());
 
 //Get All Posts
 //Future postControllerExample
 //router.get('/', postController.getUsers)
 //class PostController {
-    //static getUsers = async (req, res) => {
-    //   try {
-    //     const users = await User.find({});
-    //     res.json(users);
-    //   }
-    //   catch (err) {
-    //     res.status(500).json({ error: err.message });
-    //      }
+//static getUsers = async (req, res) => {
+//   try {
+//     const users = await User.find({});
+//     res.json(users);
+//   }
+//   catch (err) {
+//     res.status(500).json({ error: err.message });
+//      }
 //  };
 //}
 
 //export default postController
-router.get('/', async (req, res) => {
-    try {
-        const posts = await Post.find({});
-        logger.info('Fetched all posts');
-        res.json(posts);
-    } catch (err) {
-        logger.error(`Error fetching posts: ${err.message}`);
-        res.status(500).json({ error: err.message });
-    }
-})
-
-
-//Saving post information through middleware
-router.param('id', async (req,res,next, id) => {
-    try {
-        const post = await Post.findById(id);
-        if (!post) {
-            logger.warn(`Post not found with ID: ${id}`);
-            return res.status(404).json({ message: 'Post not found' });
-        }
-        req.post = post;
-        next();
-    } catch (error) {
-        logger.error(`Invalid post ID: ${id}\n${error}`);
-        return res.status(400).json({ message: `Invalid post ID` });
-    }
-})
-
-//Get A Post
-router.get('/:id', (req, res) => {
-    logger.info(`Fetched post with ID: ${req.post._id}`);
-    res.status(200).json(req.post);
-})
-
-//Create Post
-router.post('/', async (req,res) => {
-    try {
-        if (Object.keys(req.body).length === 0) {
-            logger.error('Attempted to create post with empty request body');
-            return res.status(400).json({ error: 'Empty request body' });
-        }
-
-        const newPost = await Post.create(req.body);
-        logger.info(`Created new post with ID: ${newPost._id}`);
-        res.status(201).json({
-            message: 'Post created successfully',
-            post: newPost,
-        });
-    } catch (error) {
-        logger.error(`Server error creating post: ${error.message}`);
-        res.status(500).json({ error: 'Server error creating post' });
-    }
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    logger.info("Fetched all posts");
+    res.json(posts);
+  } catch (err) {
+    logger.error(`Error fetching posts: ${err.message}`);
+    res.status(500).json({ error: err.message });
+  }
 });
 
+//Saving post information through middleware
+router.param("id", async (req, res, next, id) => {
+  try {
+    const post = await Post.findById(id);
+    if (!post) {
+      logger.warn(`Post not found with ID: ${id}`);
+      return res.status(404).json({ message: "Post not found" });
+    }
+    req.post = post;
+    next();
+  } catch (error) {
+    logger.error(`Invalid post ID: ${id}\n${error}`);
+    return res.status(400).json({ message: `Invalid post ID` });
+  }
+});
+
+//Get A Post
+router.get("/:id", (req, res) => {
+  logger.info(`Fetched post with ID: ${req.post._id}`);
+  res.status(200).json(req.post);
+});
+
+//Create Post
+router.post("/", async (req, res) => {
+  try {
+    if (Object.keys(req.body).length === 0) {
+      logger.error("Attempted to create post with empty request body");
+      return res.status(400).json({ error: "Empty request body" });
+    }
+
+    const newPost = await Post.create(req.body);
+    logger.info(`Created new post with ID: ${newPost._id}`);
+    res.status(201).json({
+      message: "Post created successfully",
+      post: newPost,
+    });
+  } catch (error) {
+    logger.error(`Server error creating post: ${error.message}`);
+    res.status(500).json({ error: "Server error creating post" });
+  }
+});
 
 //Add comment
-router.post('/:id/comments', async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        post.comments.push(req.body);
-        await post.save();
-        logger.info(`Added comment to post ${req.params.id}`);
-        res.status(201).json({ message: 'Comment added', post });
-    } catch (error) {
-        logger.error(`Error adding comment: ${error.message}`);
-        res.status(500).json({ error: error.message });
-    }
-})
+router.post("/:id/comments", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    post.comments.push(req.body);
+    await post.save();
+    logger.info(`Added comment to post ${req.params.id}`);
+    res.status(201).json({ message: "Comment added", post });
+  } catch (error) {
+    logger.error(`Error adding comment: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+});
 
 //Add like
-router.post('/:id/upvotes', async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        post.upvotes.push({ userID: req.body.userID });
-        await post.save();
-        logger.info(`Added upvote to post ${req.params.id} by user ${req.body.userID}`);
-        res.status(201).json({ message: 'Post upvoted', post });
-    } catch (error) {
-        logger.error(`Error adding upvote: ${error.message}`);
-        res.status(500).json({ error: error.message });
-    }
-})
+router.post("/:id/upvotes", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    post.upvotes.push({ userID: req.body.userID });
+    await post.save();
+    logger.info(
+      `Added upvote to post ${req.params.id} by user ${req.body.userID}`,
+    );
+    res.status(201).json({ message: "Post upvoted", post });
+  } catch (error) {
+    logger.error(`Error adding upvote: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+});
 
 export default router;
--- ElevateEatsAPI/utils/db.js
+++ ElevateEatsAPI/utils/db.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
-import logger from '../utils/logger.js'
+import logger from "../utils/logger.js";
 
 const connectDB = async (URI) => {
-    try{
-        const connect = await mongoose.connect(URI);
-        logger.info(`MongoDB Connected ${connect.connection.host}`);
-    } catch (error) {
-        logger.error(`Error connecting to MongoDB ${error.message}`)
-        process.exit(1)
-    }
+  try {
+    const connect = await mongoose.connect(URI);
+    logger.info(`MongoDB Connected ${connect.connection.host}`);
+  } catch (error) {
+    logger.error(`Error connecting to MongoDB ${error.message}`);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
